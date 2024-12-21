9dfd49afb9f3221bff82a5637217a4b91cbcc9af
Meetchandak18 <temp.free.email@gmail.com>
2024-12-21 18:32:33 +0530

    First Commit

---
+++ Test.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter the first number : ");
+		int a = sc.nextInt();
+		
+		System.out.println("Enter the second number : ");
+		int b = sc.nextInt();
+		
+		System.out.println("Enter the third number : ");
+		int c = sc.nextInt();
+		System.out.println("Value of a : "+a);
+    }
+}
